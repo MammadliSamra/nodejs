0254d57e51e30cd4496b83a6d2891b7b9e25242a
Samra Mammadli <memmedlisemra6@gmail.com>
2024-06-15 15:03:07 +0400

    Add routing

---
+++ sorting/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const cors = require('cors');
+const workersRoutes = require('./workers')
+
+const app = express()
+const port = 3000
+
+app.use('/workers', workersRoutes);
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
